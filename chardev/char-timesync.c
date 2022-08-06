c4f0db3fcefb585c6e9a4fac818222f2d757fece
Cel A. Skeggs <cela@mit.edu>
2022-08-05 20:10:10 -0700

    Commit a documentation fix from months ago

--- chardev/char-timesync.c
+++ chardev/char-timesync.c
@@ -1,7 +1,7 @@
 /*
  * QEMU System Emulator
  *
- * Copyright (c) 2003-2008 Fabrice Bellard
+ * Copyright (c) 2021
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
