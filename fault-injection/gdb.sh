a4e8ab9252e0b426c0622e8ce35a9d4e0d968f23
Haoda Wang <haoda.wang@jpl.nasa.gov>
2023-02-06 14:24:03 -0500

    Split fault injection run into multiple parts

--- fault-injection/gdb.sh
+++ fault-injection/gdb.sh
@@ -3,15 +3,7 @@ set -e -u
 # use this command: set {int}0x800000 = N
 # -s kernel
 
-# Run QEMU and load to root shell
-qemu-hce/build/qemu-system-arm -s -M virt -m 1G -nographic \
-                               -drive file="debian-11-rt/debian.qcow2",id=hd,if=none,media=disk \
-                               -device virtio-blk-device,drive=hd \
-                               -kernel debian-11-rt/vmlinuz -initrd debian-11-rt/initrd.img \
-                               -append "root=/dev/vda2" \
-                               -plugin ./qemu-hce/build/contrib/plugins/libcache.so -d plugin \
-                               -loadvm root_shell
-
+# Run GDB on QEMU instance
 HERE="$(dirname "$0")"
 gdb-multiarch -ex 'target remote :1234' \
               -ex 'maintenance packet Qqemu.PhyMemMode:1' \
---
+++ fault-injection/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e -u
+
+# Run tmux and start QEMU and GDB
+HERE="$(dirname "$0")"
+tmux \
+    new-session  '$HERE/qemu.sh' \; \
+    split-window '$HERE/gdb.sh' \; \
